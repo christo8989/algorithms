e1d137a3a7e68d758ac5b1cacc55e380cfb3c630
christo8989 <christo8989@gmail.com>
2019-04-04 15:32:55 -0700

    save files

---
+++ heap/index.js
@@ -0,0 +1,9 @@
+const random = () => { return Math.floor(Math.random() * 10) }
+const numbers = Array.from({ length: 10 }, () => random());
+console.log(numbers.join(" "));
+
+const heap = () => {
+
+}
+
+console.log(numbers.join(" "))
