7c6bcd5e6169530440c62a09a4350fc83125951a
christo8989 <christo8989@gmail.com>
2019-04-04 20:41:01 -0700

    add heap sort

--- sorting/heap/index.js
+++ sorting/heap/index.js
@@ -1,9 +1,36 @@
-const random = () => { return Math.floor(Math.random() * 10) }
-const numbers = Array.from({ length: 10 }, () => random());
-console.log(numbers.join(" "));
+const left = (index) => (index * 2 + 1)
+const right = (index) => (left(index) + 1)
+const parent = (index) => (Math.floor(index / 2 - 0.5))
 
-const heap = () => {
-
+const max_heapify = (arr, i) => {
+  let l = left(i)
+  let r = right(i)
+  let heapLength = arr.heapLength
+  let largest = l < heapLength && arr[l] >= arr[i] ? l : i;
+  largest = r < heapLength && arr[r] >= arr[largest] ? r : largest;
+  if (largest !== i) {
+    const iValue = arr[i]
+    arr[i] = arr[largest]
+    arr[largest] = iValue;
+    max_heapify(arr, largest)
+  }
 }
 
-console.log(numbers.join(" "))
+const build_max_heap = (arr) => {
+  const start = Math.floor(((arr.length - 1) / 2) - 0.5)
+  for (let i = start; i >= 0; --i) {
+    max_heapify(arr, i)
+  }
+}
+
+export const heap_sort = (arr) => {
+  arr.heapLength = arr.length
+  build_max_heap(arr)
+  for (let i = arr.length - 1; i > 0; --i) {
+    const largeValue = arr[0]
+    arr[0] = arr[i]
+    arr[i] = largeValue
+    arr.heapLength = arr.heapLength - 1
+    max_heapify(arr, 0)
+  }
+}
--- sorting/index.js
+++ sorting/index.js
@@ -2,8 +2,15 @@ import { time } from "../tools/clock"
 import { randoms } from "../tools/random"
 import { merge_sort } from "./merge/index"
 import { insertionSort } from "./insertion/index"
+import { heap_sort } from "./heap/index"
 
 const numbers = randoms(5000, 0, 999);
+
+
+const heapNumbers = numbers.slice(0, numbers.length);
+time(() => {
+  heap_sort(heapNumbers)
+}, "Heap Sort")
 
 
 const mergeNumbers = numbers.slice(0, numbers.length);
@@ -16,3 +23,9 @@ const insertionNumbers = numbers.slice(0, numbers.length);
 time(() => {
   insertionSort(insertionNumbers)
 }, "Insertion Sort")
+
+
+const javascriptNumbers = numbers.slice(0, numbers.length)
+time(() => {
+  javascriptNumbers.sort((a, b) => a - b)
+}, "Javascript Sort")
